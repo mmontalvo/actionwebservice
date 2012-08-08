9da580c4b3a007b39720e236d91497cab0aebc89
Miguel Montalvo <mmontalvo@lciberica.es>
2012-08-08 13:22:26 +0200

    Defining attr_accessor in class_inheritable_option

--- lib/action_web_service/support/class_inheritable_options.rb
+++ lib/action_web_service/support/class_inheritable_options.rb
@@ -1,6 +1,7 @@
 class Class # :nodoc:
   def class_inheritable_option(sym, default_value=nil)
 
+    self.class.module_eval { attr_accesor sym}
     self.send("#{sym}=",default_value)
     # write_inheritable_attribute sym, default_value
 
